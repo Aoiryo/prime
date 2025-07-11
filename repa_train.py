94893c33e1392c29de42e1eb5532ca5ea201d934
likeer.xu <likeer.xu@bytedance.com>
2025-07-11 15:55:10 -0700

    set grp to none

--- repa_train.py
+++ repa_train.py
@@ -913,7 +913,7 @@ def train(config: Config, args = None):
             # we only allow to checkpoint after a outer step. For non diloco training outer step = 1 anyway
 
             do_remote = config.ckpt.remote is not None and training_progress.step % config.ckpt.remote.interval == 0
-            ckpt_manager.save(remote=do_remote, group=elastic_device_mesh.ckpt_pg, store=elastic_device_mesh.god_store)
+            ckpt_manager.save(remote=do_remote, group=None, store=elastic_device_mesh.god_store)
             log_hash_training_state(
                 config, model, inner_optimizer, diloco, metric_logger, step=training_progress.step, id="save"
             )
